a689fd28968f97de2cd1fcfebc355330ed28b1a1
Wayne Feng <wei.feng@intel.com>
2024-04-03 13:58:23 +0800

    add support of Chinese CLIP

--- app.py
+++ app.py
@@ -1,49 +1,62 @@
 from tokenizer import tokenize
+from cn_tokenizer import tokenize as cn_tokenize
 from utils import load_clip_model
 from build_index import load_embeddings, transforms
 import argparse
 import gradio as gr
 import numpy as np
 
-def query_images(text, k=4, show_score=False):
-    tokens = tokenize(text)
-    text_features = tenc.infer_new_request(tokens)
+languages = {'English': 'en', '中文': 'cn'}
+tenc = {}
+ds = {}
+index = {}
+tokenizer = {'en': tokenize, 'cn': cn_tokenize}
+INDEX_COLUMN = 'embedding'
+
+def query_images(text, lang, k=4, show_score=False):
+    if lang not in languages:
+        return []
+    l = languages[lang]
+    tokens = tokenizer[l](text)
+    text_features = tenc[l].infer_new_request(tokens)
     tfeat = text_features.to_tuple()[0]
     tfeat /= np.linalg.norm(tfeat, axis=1, keepdims=True)
-    scores, indices = index.search(tfeat, k)
+    scores, indices = index[l].search(tfeat, k)
     results = []
     if show_score:
         for s, i in zip(scores, indices):
-            results.append((ds[int(i)]['image'], f"Score: {s:#.3f}"))
+            results.append((ds[l][int(i)]['image'], f"Score: {s:#.3f}"))
     else:
         for i in map(int, indices):
-            results.append(ds[i]['image'])
+            results.append(ds[l][i]['image'])
     return results
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("OpenCLIP demo: WebUI", add_help=True)
     parser.add_argument('-d', '--device', default='CPU', help='Device to inference')
-    parser.add_argument('-m', '--text_model_path', default='models/vit_h_14_text.xml', help='Path to text encoder model')
-    parser.add_argument('-e', '--embeddings_path', default='data/vlm.pkl', help='Path to the embeddings pickle file.')
+    parser.add_argument('-m', '--text_model_path', default='models/en/vit_h_14_text.xml', help='Path to text encoder model')
+    parser.add_argument('-mc', '--text_model_path_cn', default='models/cn/vit_h_14_text.xml', help='Path to Chinese text encoder model')
+    parser.add_argument('-e', '--embeddings_path', default='data/en/vlm.pkl', help='Path to the embeddings pickle file.')
+    parser.add_argument('-ec', '--embeddings_path_cn', default='data/cn/vlm.pkl', help='Path to the Chinese embeddings pickle file.')
     parser.add_argument('-n', '--max_queries', default=20, help='Maximum number of queries')
     args = parser.parse_args()
-    txt_encoder_path = args.text_model_path
-    embeddings_path = args.embeddings_path
-    device = args.device
     max_queries = int(args.max_queries)
 
-    _, tenc = load_clip_model(None, txt_encoder_path, device)
-    ds = load_embeddings(embeddings_path)
-    index = ds.get_index('embedding')
+    _, tenc['en'] = load_clip_model(None, args.text_model_path, args.device)
+    _, tenc['cn'] = load_clip_model(None, args.text_model_path_cn, args.device)
+    ds['en'] = load_embeddings(args.embeddings_path)
+    ds['cn'] = load_embeddings(args.embeddings_path_cn)
+    index['en'] = ds['en'].get_index(INDEX_COLUMN)
+    index['cn'] = ds['cn'].get_index(INDEX_COLUMN)
 
     with gr.Blocks() as demo:
-        text = gr.Textbox(label="prompt", scale=1)
-        k = gr.Slider(minimum=1, maximum=max_queries, value=max_queries//2, step=1, label="output number")
+        with gr.Row(equal_height=True):
+            lang = gr.Dropdown(choices=[x for x in languages.keys()], value='English', scale=0)
+            text = gr.Textbox(label="prompt", scale=1)
+            #score = gr.Checkbox(value=False, scale=0)
+        k = gr.Slider(minimum=1, maximum=max_queries, value=max_queries, step=1, label="output number")
         btn = gr.Button("Query")
         gallery = gr.Gallery(label="results")
-        btn.click(fn=query_images, inputs=[text, k], outputs=gallery)
+        btn.click(fn=query_images, inputs=[text, lang, k], outputs=gallery)
 
-    try:
         demo.launch(server_port=17580, debug=True)
-    except Exception:
-        demo.launch(server_port=17580, share=True, debug=True)
--- build_index.py
+++ build_index.py
@@ -28,7 +28,7 @@ def load_embeddings(embeddings_path):
         ds = pickle.load(f)
         return ds
 
-def main(device, image_path, image_model_path, embeddings_path):
+def save_embeddings(device, image_path, image_model_path, embeddings_path):
     embeddings = build_embeddings(image_path, image_model_path, device)
     embeddings.add_faiss_index(column='embedding', metric_type=faiss.METRIC_INNER_PRODUCT)
     with open(embeddings_path, 'wb') as f:
@@ -38,8 +38,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser("OpenCLIP demo: build faiss index", add_help=True)
     parser.add_argument('-d', '--device', default='CPU', help='Device to inference')
     parser.add_argument('-i', '--image_path', default='images', help='Path to images')
-    parser.add_argument('-m', '--image_model_path', default='models/vit_h_14_visual.xml', help='Path to image encoder model')
-    parser.add_argument('-e', '--embeddings_path', default='data/embeddings.pkl', help='Path to the embeddings pickle file.')
+    parser.add_argument('-m', '--image_model_path', default='models/en/vit_h_14_visual.xml', help='Path to image encoder model')
+    parser.add_argument('-mc', '--image_model_path_cn', default='models/cn/vit_h_14_visual.xml', help='Path to Chinese image encoder model')
+    parser.add_argument('-e', '--embeddings_path', default='data/en/embeddings.pkl', help='Path to the embeddings pickle file.')
+    parser.add_argument('-ec', '--embeddings_path_cn', default='data/cn/embeddings.pkl', help='Path to the Chinese embeddings pickle file.')
     args = parser.parse_args()
-    main(args.device, args.image_path, args.image_model_path, args.embeddings_path)
+    save_embeddings(args.device, args.image_path, args.image_model_path, args.embeddings_path)
+    save_embeddings(args.device, args.image_path, args.image_model_path_cn, args.embeddings_path_cn)
 
